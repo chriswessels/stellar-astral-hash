d6eecfead5c53845e671dc501f91e573c208d919
Chris Wessels <chris@paybase.io>
2018-01-31 13:29:12 +0000

    Renamed to bin folder

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec go build -ldflags "-X main.version=$(git describe --tags --always)" main.go -o ./build/astral-hash
+exec go build -o build/astral-hash -ldflags "-X main.version=$(git describe --tags --always)" main.go
